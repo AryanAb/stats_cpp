ef9c91aef018cceac64989168d60f69addba3117
AryAb <aryanabed48@gmail.com>
2020-05-17 23:47:01 -0400

    Some optimization

--- src/Statistics.cpp
+++ src/Statistics.cpp
@@ -6,114 +6,171 @@
 #include <math.h>
 #include <vector>
 #include <algorithm>
+#include <future>
+#include <random>
+#include <chrono>
 #include "Statistics.hpp"
 
-st::oneVarStats st::getOneVarStats(std::vector<double> &arr)
+std::ostream& operator<<(std::ostream& stream, const stats::oneVarStats& data)
 {
-	std::sort(arr.begin(), arr.end());
+	stream << data.min << ", " << data.q1 << ", " << data.median << ", " << data.q3 << ", " << data.max;
+	return stream;
+}
+
+stats::oneVarStats stats::getOneVarStats(std::vector<double>* arr)
+{
+	std::sort(arr->begin(), arr->end());
 
 	oneVarStats data;
 
-	data.size = arr.size();
-	data.min = arr.front();
-	data.max = arr.back();
+	data.size = arr->size();
+	data.min = arr->front();
+	data.max = arr->back();
 
 	unsigned int m_index;
 
-	if (arr.size() % 2 != 0)
+	if (arr->size() % 2 != 0)
 	{
-		m_index = (arr.size() - 1) / 2;
-		data.median = arr.at(m_index);
+		m_index = (arr->size() - 1) / 2;
+		data.median = arr->at(m_index);
 	}
 	else
 	{
-		data.median = (arr.at(arr.size() / 2 - 1) + arr.at(arr.size() / 2)) / 2;
+		data.median = (arr->at(arr->size() / 2 - 1) + arr->at(arr->size() / 2)) / 2;
 	}
 
-	st::IQR(arr, data);
+	stats::IQR(arr, &data);
 
-	double sum = 0;
-	for (double i : arr)
-	{
-		sum += i;
-	}
-
-	data.sum = sum;
-	double mean = sum / arr.size();
+	data.sum = stats::complexSum(arr);
+	double mean = data.sum / arr->size();
 
 	double totalDeviation = 0;
-	for (double i : arr)
+	for (double i : *arr)
 	{
 		totalDeviation += pow((i - mean), 2);
 	}
 
 	data.mean = mean;
-	data.std = sqrt(totalDeviation / (arr.size() - 1));
+	data.std = sqrt(totalDeviation / (arr->size() - 1));
 
 	return data;
 }
 
-unsigned int st::median(unsigned int l, unsigned int r)
+double stats::simpleSum(const std::vector<double>* arr)
+{
+	double sum = 0;
+	for (double num : *arr)
+	{
+		sum += num;
+	}
+
+	return sum;
+}
+
+// Improved Kahan–Babuška algorithm
+double stats::complexSum(const std::vector<double>* arr)
+{
+	double sum = 0.0;
+	double c = 0.0;
+	for (double num : *arr)
+	{
+		double t = sum + num;
+		if (abs(sum) > abs(num))
+		{
+			c += ((sum - t) + num);
+		}
+		else
+		{
+			c += ((num - t) + sum);
+		}
+
+		sum = t;
+	}
+	return sum + c;
+}
+
+unsigned int stats::median(unsigned int l, unsigned int r)
 {
 	int n = r - l + 1;
 	n = (n + 1) / 2 - 1;
 	return n + l;
 }
 
-void st::IQR(std::vector<double> &arr, st::oneVarStats &data)
+void stats::IQR(const std::vector<double>* arr, stats::oneVarStats* data)
 {
-	unsigned int mid_index = st::median(0, arr.size());
+	unsigned int mid_index = stats::median(0, arr->size());
 
 	if (mid_index % 2 == 0)
 	{
-		unsigned int index = st::median(0, mid_index);
-		double Q1 = (arr.at(index - 1) + arr.at(index)) / 2;
-		data.q1 = Q1;
+		unsigned int index = stats::median(0, mid_index);
+		double Q1 = (arr->at(index - 1) + arr->at(index)) / 2;
+		data->q1 = Q1;
 	}
 	else
 	{
-		double Q1 = arr.at(st::median(0, mid_index));
-		data.q1 = Q1;
+		double Q1 = arr->at(stats::median(0, mid_index));
+		data->q1 = Q1;
 	}
 
 	if (mid_index % 2 == 0)
 	{
-		unsigned int index = st::median(mid_index + 1, arr.size());
-		double Q3 = (arr.at(index - 1) + arr.at(index)) / 2;
-		data.q3 = Q3;
+		unsigned int index = stats::median(mid_index + 1, arr->size());
+		double Q3 = (arr->at(index - 1) + arr->at(index)) / 2;
+		data->q3 = Q3;
 	}
 	else
 	{
-		double Q3 = arr.at(st::median(mid_index + 1, arr.size()));
-		data.q3 = Q3;
+		double Q3 = arr->at(stats::median(mid_index + 1, arr->size()));
+		data->q3 = Q3;
 	}
-	data.iqr = data.q3 - data.q1;
+	data->iqr = data->q3 - data->q1;
 }
 
-double st::calcZScore(double value, double mean, double std)
+double stats::calcZScore(double value, double mean, double std)
 {
 	return (value - mean) / std;
 }
-double st::calcZScore(double value, st::oneVarStats stats)
+double stats::calcZScore(double value, stats::oneVarStats stats)
 {
-	return st::calcZScore(value, stats.mean, stats.std);
+	return stats::calcZScore(value, stats.mean, stats.std);
 }
 
-double st::calcZScore(double value, std::vector<double> values)
+double stats::calcZScore(double value, std::vector<double>* values)
 {
-	st::oneVarStats stats = st::getOneVarStats(values);
+	stats::oneVarStats data = stats::getOneVarStats(values);
 
-	return st::calcZScore(value, stats.mean, stats.std);
+	return stats::calcZScore(value, data.mean, data.std);
 }
 
 // https://stackoverflow.com/questions/2328258/cumulative-normal-distribution-function-in-c-c
-double st::normalCDF(double value)
+double stats::normalCDF(double value)
 {
 	return 0.5 * std::erfc(-value * SQRT_1_2);
 }
 
+/*void stats::timer(std::vector<double>* nums, p_getOneVarStats func)
+{
+	auto start = std::chrono::high_resolution_clock::now();
+	func(nums);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> duration = end - start;
+
+	std::cout << duration.count() << std::endl;
+}*/
+
+
+
 int main()
 {
+
+	std::vector<double> nums;
+
+	for (int i = 0; i < 1000000; i++)
+	{
+		nums.push_back(std::rand() % 10000);
+	}
+
+	stats::timer(&nums, stats::getOneVarStats);
 
 	return 0;
 }
--- src/Statistics.hpp
+++ src/Statistics.hpp
@@ -2,14 +2,15 @@
 
 #include <vector>
 
-namespace st
+namespace stats
 {
+
 	struct oneVarStats
 	{
 		double sum;
 		double mean;
 		double std;
-		size_t size;
+		unsigned int size;
 		double min;
 		double q1;
 		double median;
@@ -18,15 +19,32 @@ namespace st
 		double iqr;
 	};
 
-	oneVarStats getOneVarStats(std::vector<double> &arr);
+	double simpleSum(const std::vector<double> *arr);
+	double complexSum(const std::vector<double> *arr);
+
+	oneVarStats getOneVarStats(std::vector<double> *arr);
 	unsigned int median(unsigned int l, unsigned int r);
-	void IQR(std::vector<double> &arr, st::oneVarStats &data);
+	void IQR(const std::vector<double> *arr, stats::oneVarStats *data);
 
 	double calcZScore(double value, double mean, double std);
 	double calcZScore(double value, oneVarStats stats);
-	double calcZScore(double value, std::vector<double> values);
+	double calcZScore(double value, std::vector<double> *values);
 
 	// calculates cdf using error function
 	double normalCDF(double value);
 
-} // namespace st
+	//using p_getOneVarStats = stats::oneVarStats(*)(std::vector<double>*);
+	//void timer(std::vector<double>* nums, p_getOneVarStats func);
+
+	template <typename T, typename F>
+	void timer(T inputs, F func)
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		func(inputs);
+		auto end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<float> duration = end - start;
+
+		std::cout << duration.count() << std::endl;
+	}
+
+} // namespace stats
